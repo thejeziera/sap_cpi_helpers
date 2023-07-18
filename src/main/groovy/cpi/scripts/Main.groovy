883b9b57b1c1943465f7c13ab5c7efcb94f24151
Michal Jezierski <mje@nesim.pl>
2023-07-18 14:58:49 +0200

    Repo migration

---
+++ src/main/groovy/cpi/scripts/Main.groovy
@@ -0,0 +1,9 @@
+package cpi.scripts
+
+import com.sap.gateway.ip.core.customdev.processor.MessageImpl
+import com.sap.gateway.ip.core.customdev.util.Message
+
+static void main(String[] args) {
+    println "Hello world!"
+    def Message testMessage = new MessageImpl();;
+}
