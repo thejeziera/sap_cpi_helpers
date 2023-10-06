222d7566b403ca09e9e4f85c9f5f8e3fec09aded
thejeziera <jezierskimichal1@gmail.com>
2023-10-06 18:40:22 +0200

    LogMessage expanded

--- src/main/groovy/cpi/scripts/monitoring/LogMessage.groovy
+++ src/main/groovy/cpi/scripts/monitoring/LogMessage.groovy
@@ -5,17 +5,45 @@ import com.sap.gateway.ip.core.customdev.util.Message
 def Message processData(Message message) {
     def messageLog = messageLogFactory.getMessageLog(message)
 
-    message.getHeaders().each{
+    message.getHeaders().each {
         entry ->
-            messageLog?.addCustomHeaderProperty("HEADER_" + entry.key, entry.value)
+            messageLog?.addCustomHeaderProperty("HEADER_" + entry.key, entry.value as String)
     }
 
-    message.getProperties().each{
+    message.getProperties().each {
         entry ->
-            messageLog?.addCustomHeaderProperty("PROPERTY_" + entry.key, entry.value)
+            messageLog?.addCustomHeaderProperty("PROPERTY_" + entry.key, entry.value as String)
     }
 
-    messageLog?.addAttachmentAsString("Message Body", message.getBody(), "text/plain")
+    message.exchange.getIn().getHeaders().each {
+        entry ->
+            messageLog?.addCustomHeaderProperty("EX_IN_HEADER_" + entry.key, entry.value as String)
+    }
+
+    message.exchange.getProperties().each {
+        entry ->
+            messageLog?.addCustomHeaderProperty("EX_PROP_" + entry.key, entry.value as String)
+    }
+
+    message.attachments.each {
+        messageLog?.addCustomHeaderProperty("ATTACHMENT_" + entry.key, entry.value as String)
+    }
+
+    message.attachmentObjects.each {
+        messageLog?.addCustomHeaderProperty("ATT_OBJECT" + entry.key, entry.value as String)
+    }
+
+    message.attachmentWrapperObjects.each {
+        messageLog?.addCustomHeaderProperty("ATT_WRAP_OBJ" + entry.key, entry.value as String)
+    }
+
+    messageLog?.addCustomHeaderProperty("TEST_LOG", message.log as String)
+    messageLog?.addCustomHeaderProperty("TEST_EXCHANGE", message.exchange as String)
+    messageLog?.addCustomHeaderProperty("TEST_SAP_SOAP_HEADER_WRITE", message.SAP_SOAP_HEADER_WRITE as String)
+    messageLog?.addCustomHeaderProperty("TEST_SAP_SOAP_HEADER_READ", message.SAP_SOAP_HEADER_READ as String)
+
+    messageLog?.addAttachmentAsString("Message Body", message.payload as String, "text/plain")
+    messageLog?.addAttachmentAsString("Exchange Message Body", message.exchange.getIn().getBody() as String, "text/plain")
 
     return message
 }
